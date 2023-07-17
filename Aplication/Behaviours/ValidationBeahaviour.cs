e8a0cf16985f28ace8cfe499509a7a5546775b34
3GATO3 <chaxgu@gmail.com>
2023-07-17 15:06:19 -0600

    Add project files.

---
+++ Aplication/Behaviours/ValidationBeahaviour.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Behaviours
+{
+    public class ValidationBeahaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBeahaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        { 
+
+            if (_validators.Any()) {
+            var context = new FluentValidation.ValidationContext<TRequest>(request);
+                var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+                var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();  
+
+                if (failures.Count != 0) {
+                    throw new Exceptions.ValidationException(failures);
+                }
+            }
+
+            return await next();
+        }
+    }
+}
